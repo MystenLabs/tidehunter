e7fee0a58101d4370f954466135978cc99646042
Igor Zablotchi <igor.zablotchi@gmail.com>
2025-03-04 18:16:27 -0700

    Refactoring: moved benchmarks to their own folder

---
+++ src/benchmarks/mod.rs
@@ -0,0 +1,6 @@
+#[cfg(feature = "random_access_speed_test")]
+pub mod random_access_speed_test;
+#[cfg(feature = "read_window_test")]
+pub mod read_window_test;
+#[cfg(feature = "stress")]
+pub mod stress;
